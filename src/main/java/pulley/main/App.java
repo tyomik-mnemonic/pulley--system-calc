26cfa400f36c3c4a84767116b3ed02c186a1b7bd
Комаров Артём Вячеславович <komarov_av3@nlmk.com>
2022-11-18 23:18:33 +0300

    init commit

---
+++ src/main/java/pulley/main/App.java
@@ -0,0 +1,9 @@
+package pulley.main;
+import pulley.main.DummyGuyMass;
+
+public class App {
+    public static void main(String args[]){
+        DummyGuyMass weight = new DummyGuyMass(100.0);
+        System.out.println("there is main method");
+    }
+}
---
+++ src/main/java/pulley/main/DummyGuyMass.java
@@ -0,0 +1,14 @@
+package pulley.main;
+
+public class DummyGuyMass {
+    double weight;
+
+    public  DummyGuyMass(double value){
+        weight = value;
+        System.out.println("class was inited");
+    }
+
+    public static void main(String[] args){
+        System.out.println("there is main method-0");
+    }
+}
